c0bd58bbe98f302ae9a2fefafe04ea4ab7bec42c
Mohdkasimkhan <141662332+Mohdkasimkhan@users.noreply.github.com>
2024-09-24 10:37:05 +0530

    Update string.js

--- string.js
+++ string.js
@@ -1,1 +1,20 @@
+//String.js
+
+const name = "Ritesh chauhan
+const repocount = 50
+//console.log(hello my name is ${name} and my repocount is ${repocount})
+const GameName = new String("Riteshch")
+console.log(GameName)
+console.log(GameName.length)
+console.log(GameName.charAt('3'));
+console.log(GameName.slice(2,7));
+console.log(GameName.concat(name));
+console.log(GameName.split())
+console.log(GameName.trim(2 ,4))
+console.log(GameName.toLowerCase());
+//console.log(GameName.replace)
+console.log(GameName.valueOf())
+console.log(GameName+name);
+console.log(GameName.substr(2,1));
+console.log(GameName+name)
 
