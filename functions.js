ab56be53a59c6b6cddae73f9f7bbc17642812ac1
Mohdkasimkhan <141662332+Mohdkasimkhan@users.noreply.github.com>
2024-09-24 10:36:28 +0530

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,1 +1,38 @@
 
+
+console.log("H")
+console.log("H")
+console.log("H")
+console.log("H")
+console.log("H")
+console.log("H");
+function myname(){
+    console.log("H")
+console.log("H")
+console.log("H")
+console.log("H")
+console.log("H")
+console.log("H");
+
+}
+//myname()
+function add2(number1,number2){
+    console.log(number1+number2)
+
+}
+add2(3,"a")
+function loginUser(username){
+    return `${username}just logged in`
+}
+console.log(loginUser("kasim khan"))
+function calculatecarprice(val1, val2,...num1){
+return num1
+}
+//console.log(calculatecarprice(200,400,500,2000))
+const user={
+    username:"kasim khan"
+    price:199
+}
+function handleobject(anyobject){
+    console.log(username $)
+}
